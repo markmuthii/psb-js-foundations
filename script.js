9314adef5fdcfd6935fc815bbe4e3b7e62d1c734
Mark Muthii <mark.muthii@strathmore.edu>
2025-05-26 11:52:16 +0300

    Adding js, comments, console.log

---
+++ script.js
@@ -0,0 +1,13 @@
+// single line comment
+
+/** 
+multi-line
+comments
+
+Used to add some documentation to functions and classes
+*/
+function sampleFunction() {}
+
+console.log("Hello, World");
+
+sampleFunction();
