712da8f223fbd088a2083223026fc996f440935c
Mark Muthii <mark.muthii@strathmore.edu>
2025-05-26 13:31:16 +0300

    Variables

---
+++ variables.js
@@ -0,0 +1,28 @@
+// 1. Declaration
+
+// Declaring variable in JAVASCRIPT
+// var
+// let
+// const
+
+// a. var
+var variableName;
+console.log(variableName);
+
+variableName = "Hey There";
+console.log(variableName);
+
+// b. let
+let variableName2;
+console.log(variableName2);
+
+variableName2 = "Let variable";
+console.log(variableName2);
+
+// c. const
+const variableName3 = "Hello, world!";
+console.log(variableName3);
+
+// 2. Assignment
+
+// 3. Initialization
