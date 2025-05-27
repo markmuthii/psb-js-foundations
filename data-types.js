546cab65b37b4f14f23a65e3e4ec9d0ed65fc070
Mark Muthii <mark.muthii@strathmore.edu>
2025-05-27 15:20:54 +0300

    Data types

---
+++ data-types.js
@@ -0,0 +1,37 @@
+// Type of the value that is stored in a variable
+
+// 1. Number (any positive or negative integer, including floats/decimals)
+let num = 7;
+num = 7.0;
+num = -7;
+num = 9007199254740991;
+console.log(typeof num);
+
+console.log(7 - "7");
+
+// Addition
+// Subtraction
+// Multiplication
+// Division
+// Remainders
+
+// 2. String (sequence of alphanumeric characters and/or symbols enclosed in double/single quotations or backticks)
+// double quotation ("")
+// single quotation ('')
+// backticks (``)
+let str = `___@--*^&#$@#!@%^&)*({}sjdfhhsdjfhkjh34kj7568348754j56hkjhjkshfd`;
+console.log(typeof str);
+console.log(1 + "1"); // concatenation
+
+// 3. Booleans (true/false)
+let bool = true;
+bool = false;
+console.log(typeof bool);
+
+// 4. Null
+let n = null;
+console.log(typeof n);
+
+// 5. Undefined
+let u = undefined;
+console.log(typeof u);
