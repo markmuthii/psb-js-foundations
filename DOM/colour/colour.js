c51d3426b0b1797869c40c19460fc33dabf2f21d
Mark Muthii <mark.muthii@strathmore.edu>
2025-07-02 17:57:31 +0300

    Update comments

--- DOM/colour/colour.js
+++ DOM/colour/colour.js
@@ -1,6 +1,9 @@
+// Get the first element with class 'box'
 const box = document.getElementsByClassName("box")[0];
+// Get the first button element on the page
 const button = document.getElementsByTagName("button")[0];
 
+// Array of possible background colours
 const colours = [
   "red",
   "green",
@@ -12,23 +15,27 @@ const colours = [
   "yellow",
 ];
 
+// Array of CSS class names for colour themes
 const classes = ["red", "green", "blue"];
 
+// Returns a random integer between min and max (inclusive)
 function getRandomWholeNumberInRange(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+// Example: Change box background colour directly using style (commented out)
 // button.addEventListener("click", () => {
 //   box.style = `background-color: ${
 //     colours[getRandomWholeNumberInRange(0, colours.length - 1)]
 //   }`;
 // });
 
+// When the button is clicked, reset box classes and add a random colour class
 button.onclick = () => {
-  box.classList = "";
+  box.classList = ""; // Remove all existing classes
 
   box.classList.add(
-    "box",
-    classes[getRandomWholeNumberInRange(0, classes.length - 1)]
+    "box", // Always keep the 'box' class
+    classes[getRandomWholeNumberInRange(0, classes.length - 1)] // Add a random colour class
   );
 };
