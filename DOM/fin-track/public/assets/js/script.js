2761fa266d4c271d13ed61a56c6db1c62b9bcf1c
Mark Muthii <mark.muthii@strathmore.edu>
2025-07-10 18:04:49 +0300

    Fintrack: Load transactions, add localstorage, filter transactions

--- DOM/fin-track/public/assets/js/script.js
+++ DOM/fin-track/public/assets/js/script.js
@@ -3,6 +3,10 @@ let balance = 0; // Current balance
 let income = 0; // Total income
 let expenses = 0; // Total expenses
 
+let transactions = [];
+
+let filteredTransactions = [];
+
 // Get references to the DOM elements displaying the values
 const balanceEl = document.querySelector("#balance");
 const incomeEl = document.querySelector("#income");
@@ -11,11 +15,93 @@ const expensesEl = document.querySelector("#expenses");
 // Reference to the transaction form
 const addTransactionForm = document.querySelector("form");
 
+const transactionsTableBody = document.getElementById(
+  "transactions-table-body"
+);
+
+const typeFilterEl = document.getElementById("filter-type");
+const descriptionFilterEl = document.getElementById("filter-description");
+const dateRangeFilterEl = document.getElementById("flatpickr-range");
+
 // Loads the current balances into the DOM
 const loadBalances = () => {
   balanceEl.textContent = balance;
   incomeEl.textContent = income;
   expensesEl.textContent = expenses;
+};
+
+const loadTransactions = (transactions) => {
+  let output = "";
+
+  transactions.forEach((transaction, i) => {
+    output += `
+      <tr
+        class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200"
+      >
+        <th
+          scope="row"
+          class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+        >
+          ${transaction.id}
+        </th>
+        <td class="px-6 py-4">${transaction.amount}</td>
+        <td class="px-6 py-4">${transaction.date}</td>
+        <td class="px-6 py-4">${transaction.trxType}</td>
+        <td class="px-6 py-4">${transaction.description}</td>
+      </tr>
+    `;
+  });
+
+  transactionsTableBody.innerHTML =
+    output || "<p class='text-center'>No transactions</p>";
+};
+
+const filterTransactions = (type, description, dateRange) => {
+  filteredTransactions = transactions;
+
+  if (type) {
+    filteredTransactions =
+      type === "all"
+        ? filteredTransactions
+        : filteredTransactions.filter((transaction) => {
+            return (
+              transaction.trxType === type[0].toUpperCase() + type.slice(1)
+            );
+          });
+  }
+
+  if (description) {
+    filteredTransactions = filteredTransactions.filter((transaction) => {
+      return transaction.description
+        .toLowerCase()
+        .includes(description.toLowerCase());
+    });
+  }
+
+  if (dateRange) {
+    const dates = dateRange.split(" to ");
+
+    const [start, end] = dates;
+
+    filteredTransactions = filteredTransactions.filter((transaction) => {
+      let [day, month, year] = transaction.date.split("/");
+      month = parseInt(month) - 1;
+
+      const trxDate = new Date(year, month, day);
+
+      const startDate = new Date(start);
+      const endDate = new Date(end);
+
+      console.log({ startDate, endDate, trxDate });
+
+      return (
+        trxDate.getTime() >= startDate.getTime() &&
+        trxDate.getTime() <= endDate.getTime()
+      );
+    });
+  }
+
+  loadTransactions(filteredTransactions);
 };
 
 // Handles form submission for adding a transaction
@@ -62,12 +148,73 @@ const formSubmitted = (e) => {
     expenses += amount;
   }
 
+  // income / expense
+  // Income / Expense
+
+  console.log(trxType[0].toUpperCase() + trxType.slice(1));
+
+  console.log(new Date().toLocaleDateString());
+
+  const transaction = {
+    id: transactions.length + 1,
+    date: new Date().toLocaleDateString(),
+    amount,
+    trxType: trxType[0].toUpperCase() + trxType.slice(1),
+    description,
+  };
+
+  transactions.push(transaction);
+
+  localStorage.setItem(
+    "cashFlow",
+    JSON.stringify({
+      balance,
+      income,
+      expenses,
+    })
+  );
+
+  localStorage.setItem("transactions", JSON.stringify(transactions));
+
   // Update the DOM with new values
   loadBalances();
+
+  loadTransactions(transactions);
 };
 
 // Initialize balances on page load
-window.onload = loadBalances;
+window.onload = () => {
+  const cashFlow = JSON.parse(localStorage.getItem("cashFlow"));
+  transactions = JSON.parse(localStorage.getItem("transactions") || "[]");
+  filteredTransactions = transactions;
+
+  console.log(transactions);
+
+  balance = cashFlow?.balance || 0;
+  income = cashFlow?.income || 0;
+  expenses = cashFlow?.expenses || 0;
+
+  loadBalances();
+  loadTransactions(transactions);
+};
 
 // Attach form submission handler
 addTransactionForm.onsubmit = formSubmitted;
+
+typeFilterEl.onchange = (e) => {
+  filterTransactions(e.target.value, descriptionFilterEl.value);
+};
+
+descriptionFilterEl.onkeyup = (e) => {
+  filterTransactions(typeFilterEl.value, e.target.value);
+};
+
+dateRangeFilterEl.onchange = (e) => {
+  if (e.target.value.includes("to")) {
+    filterTransactions(
+      typeFilterEl.value,
+      descriptionFilterEl.value,
+      e.target.value
+    );
+  }
+};
